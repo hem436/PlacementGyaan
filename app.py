b189d990549c8bc0574cd7d16c0a2c1f1b06975a
smhemant <hemant436268s@gmail.com>
2023-08-28 22:51:41 +0530

    added evaluation

--- app.py
+++ app.py
@@ -43,36 +43,45 @@ def check_username(name):
         return False
     return True
 
-# import nltk
-# from nltk.corpus import stopwords
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# nltk.download('punkt')
-# nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('stopwords')
 
-# # Sample correct answer and student answer
-# correct_answer = "The capital of France is Paris."
-# student_answer = "Paris is the capital of France."
+# Sample correct answer and student answer
+correct_answer = "The capital of France is Paris."
+student_answer = "Germany is not the capital of Mexico."
 
-# # Preprocess the text data
-# def preprocess_text(text):
-#     words = nltk.word_tokenize(text.lower())
-#     words = [word for word in words if word.isalnum() and word not in stopwords.words('english')]
-#     return ' '.join(words)
+# Preprocess the text data
+def preprocess_text(text):
+    words = nltk.word_tokenize(text.lower())
+    words = [word for word in words if word.isalnum() and word not in stopwords.words('english')]
+    return ' '.join(words)
 
-# preprocessed_correct = preprocess_text(correct_answer)
-# preprocessed_student = preprocess_text(student_answer)
+def evaluateQ(correct_answer, student_answer):
+    # Preprocess the text data
+    preprocessed_correct = preprocess_text(correct_answer)
+    preprocessed_student = preprocess_text(student_answer)
 
-# # Vectorize the text using TF-IDF
-# vectorizer = TfidfVectorizer()
-# tfidf_matrix = vectorizer.fit_transform([preprocessed_correct, preprocessed_student])
+    # Vectorize the text using TF-IDF
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform([preprocessed_correct, preprocessed_student])
 
-# # Calculate cosine similarity
-# cosine_sim = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])[0][0]
+    # Calculate cosine similarity
+    cosine_sim = cosine_similarity(tfidf_matrix[0], tfidf_matrix[1])[0][0]
 
-# # Print the cosine similarity
-# print("Cosine Similarity:", cosine_sim)
+    # Print the cosine similarity
+    print("Cosine Similarity:", cosine_sim)
+    
+    if cosine_sim > 0.5:
+        return True
+    else:
+        return False
+print(evaluateQ(correct_answer, student_answer))
+
 
 
 @app.route('/')
@@ -164,6 +173,10 @@ def assess():
 def view_assessment(apti_id):
     if request.method == 'GET':
         return render_template('view_assess.html', apti_id=apti_id)
+    
+@app.route('/connect')
+def connect():
+    return render_template('connect.html')
 
 class Login_api(Resource):
     def post(self):
